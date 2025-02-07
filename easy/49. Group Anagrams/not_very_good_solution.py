7d57e65d222cd17016de31c04236d91422c8f850
Yegor Shved <99egor.shved@gmail.com>
2025-02-07 19:19:54 +0300

    solved group anagrams, but didn't really by myself, couldn't do it so i checked a solution and understood it :)

---
+++ easy/49. Group Anagrams/not_very_good_solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, s: List[str]) -> List[List[str]]:
+        
+        result = defaultdict(list)
+
+        for i in s:
+            sortedS = ''.join(sorted(i))
+            result[sortedS].append(i)
+        return list(result.values())    

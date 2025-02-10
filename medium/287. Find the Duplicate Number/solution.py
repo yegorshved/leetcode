9487301cff7f9ae207803ce23fb53250b7148baa
Yegor Shved <99egor.shved@gmail.com>
2025-02-10 09:57:26 +0300

    287 find the duplicate number

---
+++ medium/287. Find the Duplicate Number/solution.py
@@ -0,0 +1,17 @@
+# Floyd's tortoise and hare algorithm
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        n = len(nums)
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
